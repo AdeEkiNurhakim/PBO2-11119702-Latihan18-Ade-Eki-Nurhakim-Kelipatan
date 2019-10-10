2a923017f79f58feea437e4810aac2b7127d1fdf
AdeEkiNurhakim <55829478+AdeEkiNurhakim@users.noreply.github.com>
2019-10-10 20:30:59 +0700

    Add files via upload

---
+++ PBO2-11119702-Latihan18-Ade Eki Nurhakim-Kelipatan/Kelipatan/src/kelipatan/Kelipatan.java
@@ -0,0 +1,27 @@
+/*Nama 	 : Ade Eki Nurhakim
+NIM 	 : 10119702
+Fakultas : Teknik Dan Ilmu Komputer
+Prodi 	 : Teknik Informatika
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kelipatan;
+
+/**
+ *
+ * @author Ekiw
+ */
+public class Kelipatan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+                double i = 3.5;
+                while(i<=35){
+                System.out.println(i);
+                i = i + 3.5;
+    }
+}
+}
